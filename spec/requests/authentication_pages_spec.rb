da6eb3686b1b640a2aa5619a902a456f7aedfdfd
Robert <robert3d@gmail.com>
2014-03-08 19:59:40 -0600

    Add user microposts

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -45,9 +45,9 @@ describe "Authentication" do
   describe "authorization" do
 
     describe "as non-admin user" do
+
       let(:user) { FactoryGirl.create(:user) }
       let(:non_admin) { FactoryGirl.create(:user) }
-
       before { sign_in non_admin, no_capybara: true }
 
       describe "submitting a DELETE request to the Users#destroy action" do
@@ -57,6 +57,7 @@ describe "Authentication" do
     end
 
     describe "as wrong user" do
+
       let(:user) { FactoryGirl.create(:user) }
       let(:wrong_user) { FactoryGirl.create(:user, email: "wrong@example.com") }
       before { sign_in user, no_capybara: true }
@@ -77,6 +78,7 @@ describe "Authentication" do
       let(:user) { FactoryGirl.create(:user) }
 
       describe "when attempting to visit a protected page" do
+
         before do
           visit edit_user_path(user)
           fill_in "Email", with: user.email
@@ -106,6 +108,19 @@ describe "Authentication" do
         describe "visiting the user index" do
           before { visit users_path }
           it { should have_title('Sign in') }
+        end
+      end
+
+      describe "in the Microposts controller" do
+
+        describe "submitting to the create action" do
+          before { post microposts_path }
+          specify { expect(response).to redirect_to(signin_path) }
+        end
+
+        describe "submitting to the destroy action" do
+          before { delete micropost_path(FactoryGirl.create(:micropost)) }
+          specify { expect(response).to redirect_to(signin_path) }
         end
       end
 
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,16 +1,5 @@
 include ApplicationHelper
 
-<<<<<<< HEAD
-  def valid_signin(user)
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
-  end
-
-RSpec::Matchers.define :have_error_message do |message|
-  match do |page|
-    expect(page).to have_selector('div.alert.alert-error', text: message)
-=======
   def sign_in(user, options={})
     if options[:no_capybara]
       # Sign in when not using capybara.
@@ -23,13 +12,6 @@ RSpec::Matchers.define :have_error_message do |message|
       fill_in "Password", with: user.password
       click_button "Sign in"
     end
->>>>>>> updating-users
-  end
-
-  def valid_signin(user)
-    fill_in "Email", with: user.email
-    fill_in "Password", with: user.password
-    click_button "Sign in"
   end
 
   RSpec::Matchers.define :have_error_message do |message|
